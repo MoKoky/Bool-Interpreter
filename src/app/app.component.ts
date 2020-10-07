000f747e50fb6cef34a55e760c4993ff3bea1126
Daniel <daniel.brommer@gmx.de>
2020-10-07 18:12:10 +0200

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Bool-Interpreter';
+  calc = 0;
+
+  ngOnInit(): void {
+    this.calc = 1 + 2;
+  }
 }
